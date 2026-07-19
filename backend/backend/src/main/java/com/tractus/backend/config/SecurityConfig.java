16e34b6490967acb6ef7e62617531943999ce84e
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-07-19 11:55:06 +0530

    feat: implement JWT-based security configuration and add unit tests for controllers

--- backend/backend/src/main/java/com/tractus/backend/config/SecurityConfig.java
+++ backend/backend/src/main/java/com/tractus/backend/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,9 +22,6 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
